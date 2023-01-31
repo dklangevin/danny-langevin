2560ea28cce12d8b97112dc045bd1ea9becc6b22
Danny Langevin <dklangevin@gmail.com>
2023-01-30 23:07:35 -0600

    Add gradient generation utils

---
+++ util.js
@@ -0,0 +1,22 @@
+const simpleHash = (string) => {
+  var hash = 0;
+  for (var i = 0; i < string.length; i++) {
+    hash = (hash << 5) - hash + string.charCodeAt(i);
+  }
+  return hash;
+};
+
+export const generateLinearGradientBackground = (seed) => {
+  const hash = simpleHash(seed);
+  const first = Math.abs(hash % 360);
+  const second = Math.abs(
+    (hash + Math.round(((hash % 100) / 100) * 60 + 120)) % 360
+  );
+  return `linear-gradient(to bottom right, hsl(${first} 50% 40%), hsl(${second} 50% 40%))`;
+};
+
+export const generateSolidBackground = (seed) => {
+  const hash = simpleHash(seed);
+  const first = Math.abs(hash % 360);
+  return `hsl(${first} 40% 30%)`;
+};
