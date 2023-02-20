d2d387201b8c54988277f84fff1c8ec5628e7d06
Danny Langevin <dklangevin@gmail.com>
2023-02-20 09:50:54 -0700

    Update constants

--- constants.js
+++ constants.js
@@ -203,15 +203,7 @@ export const projects = [
     shortDescription: 'Issue Tracking App',
     description:
       'Issue tracking app like JIRA, Linear, and others, built with React. This project intentionally uses very minimal JS libraries in order to gain experience implementing lower-level functions such as database modeling and querying, front-end data fetching, image-uploading, etc. This project features client and server codebases (both hosted on Netlify), elaborate database (using bit.io), and Cloudinary for image storage/hosting. Currently in development.',
-    technologies: [
-      'React.js',
-      'Axios',
-      'Node.js',
-      'Next.js',
-      'PostgreSQL',
-      'Express.js',
-      'styled-components',
-    ],
+    technologies: ['React.js', 'Axios', 'Node.js', 'PostgreSQL', 'Express.js'],
     otherTechnologies: [
       'Full Stack Application',
       'Image Uploading',
